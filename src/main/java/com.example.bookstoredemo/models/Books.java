c3ef3800ad5e7b24be809371e17294aa98c5e482
usmanov <azikusmanov882@gmail.com>
2024-02-15 15:00:04 +0500

    added fields to Books model

--- src/main/java/com.example.bookstoredemo/models/Books.java
+++ src/main/java/com.example.bookstoredemo/models/Books.java
@@ -1,4 +1,48 @@
 package com.example.bookstoredemo.models;
 
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name="books")
 public class Books {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    @Column(name = "title")
+    private String title;
+
+    @Column(name = "description")
+    private String description;
+
+    @Column(name = "image_url")
+    private String imageUrl;
+
+    @Column(name = "book_url")
+    private String bookUrl;
+
+    @Column(name = "author")
+    private String author;
+
+    @JsonIgnoreProperties({"hibernateLazyInitializer", "handler"})
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id")
+    private User publisher;
+
+    public Books(String title, String description, String imageUrl, String bookUrl, String author) {
+        this.title = title;
+        this.description = description;
+        this.imageUrl = imageUrl;
+        this.bookUrl = bookUrl;
+        this.author = author;
+    }
 }
+
